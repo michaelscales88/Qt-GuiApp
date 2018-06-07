5669cf166036be0a23077c46b135a504b3d4ba34
Michael Scales <michael.scales88@gmail.com>
2018-06-07 16:19:39 -0500

    update qmake config to separate src/header/output files

--- src/App.cpp
+++ src/App.cpp
@@ -7,11 +7,33 @@
 
 using namespace std;
 
-int main(int argv, char **args) {
-   QApplication app(argv, args);
-   QTextEdit textEdit;
+#include <QApplication>
+#include <QHBoxLayout>
+#include <QSlider>
+#include <QSpinBox>
+
+#include "test.h"
+
+using namespace std;
+
+int main(int argc, char *argv[]) {
+   QApplication app(argc, argv);
+   QWidget *window = new QWidget;
+   window->setWindowTitle("Enter Your Age");
+   QSpinBox *spinBox = new QSpinBox;
+   QSlider *slider = new QSlider(Qt::Vertical);
+   spinBox->setRange(0, 130);
+   slider->setRange(0, 130);
+   QObject::connect(spinBox, SIGNAL(valueChanged(int)),
+   slider, SLOT(setValue(int)));
+   QObject::connect(slider, SIGNAL(valueChanged(int)),
+   spinBox, SLOT(setValue(int)));
+   spinBox->setValue(35);
+   QHBoxLayout *layout = new QHBoxLayout;
+   layout->addWidget(spinBox);
+   layout->addWidget(slider);
+   window->setLayout(layout);
    cout << fnTest() << endl;
-   textEdit.show();
+   window->show();
    return app.exec();
 }
-

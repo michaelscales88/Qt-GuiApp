b8c570b4dd3c96e45aaff784298dc6a3fa68be4a
Michael Scales <16503997+michaelscales88@users.noreply.github.com>
2018-06-11 16:36:23 -0500

    Update dollars_widget.cpp

--- src/dollars_widget.cpp
+++ src/dollars_widget.cpp
@@ -1,1 +1,5 @@
 #include "dollars_widget.h"
+
+DollarsWidget::DollarsWidget() { cout << "create dollar widget" << endl; }
+
+DollarsWidget::~DollarsWidget() { cout << "destroy dollar widget" << endl; }

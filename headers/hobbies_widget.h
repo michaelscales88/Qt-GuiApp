d1f6f203c0f6b12b56eac7659586ac583ae3b42b
Michael Scales <16503997+michaelscales88@users.noreply.github.com>
2018-06-11 16:34:47 -0500

    Update hobbies_widget.h

---
+++ headers/hobbies_widget.h
@@ -0,0 +1,13 @@
+#pragma once
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+class HobbiesWidget {
+   private:
+
+   public:
+      HobbiesWidget();
+      ~HobbiesWidget();
+};
